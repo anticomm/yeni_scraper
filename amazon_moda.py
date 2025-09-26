d0d6925edc9325e248badc2e71cb1dbad12e919a
anticomm <93606441+anticomm@users.noreply.github.com>
2025-09-26 15:20:04 +0300

    Update amazon_moda.py

--- amazon_moda.py
+++ amazon_moda.py
@@ -11,7 +11,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from telegram_cep import send_message
 
-URL = "https://www.amazon.com.tr/s?k=yaz%C4%B1c%C4%B1&i=warehouse-deals&srs=44219324031&bbn=44219324031&rh=n%3A44219324031%2Cn%3A12601910031&dc&ds=v1%3ASmRQqwXhK%2FdZ%2FZYlYfFeOyFvytObwzoWIbH9%2Fvfy334&__mk_tr_TR=%C3%85M%C3%85%C5%BD%C3%95%C3%91"
+URL = "https://www.amazon.com.tr/s?i=fashion&rh=n%3A13546675031%2Cp_36%3A41000-115000%2Cp_6%3AA1UNQM1SR2CHM%2Cp_123%3A198664%257C234857%257C256097%257C6832%2Cp_n_g-1004152217091%3A13681703031%257C13681704031%257C13681705031%257C13681706031%257C13681707031&s=price-asc-rank&dc&ds=v1%3A2yjCoesrVCjvcZCCGwaMuJeEX9HFjHO0Fia1OLlpKPQ"
 COOKIE_FILE = "cookie_cep.json"
 SENT_FILE = "send_products.txt"
 
